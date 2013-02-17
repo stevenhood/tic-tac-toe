8972023f9c48e22f898ca6025f339d843a09382c
stevenhood <steven.hood@live.co.uk>
2013-02-17 12:26:26 +0000

    version 1.0 reformat
    -Reformatted source code to improve readability (indented lines with 4 spaces using Notepad++ as opposed to the 2 space indentation that Emacs inserts, which is troublesome);
    -Added brief documentation and instructions to README.md file.

--- README.md
+++ README.md
@@ -1,10 +1,37 @@
-** Tic-Tac-Toe README [16.02.2013]
+** Tic-tac-toe README [16.02.2013]
 ** by Steven Hood
 
 -A simple text-based console game written in C source code.
+
 -Project started on 15th February 2013.
 
 Development tools:
+
 -GNU Emacs 23
+
+-Notepad++ (used to reformat source code)
+
 -GNU Project C Compiler (gcc)
 
+
+HOW TO PLAY
+===========
+
+*Intro.
+  "Tic-tac-toe" or "Noughts and crosses" is based on the classic pencil-and-paper game of the same name where two players, O and X, take turns marking spaces in a 3x3 grid. The player who succeeds in placing three marks in a horizontal, vertical or diagonal row wins the game.
+
+*Playing the game
+  Compile the C source file tictactoe.c using a compiler (such as gcc) and run the executable file in a terminal. The game will start, displaying the initial state of the game, which is the empty board. Players will be referred to as "Player 1 (Noughts)" and "Player 2 (Crosses)" during gameplay.
+
+  Two players will take turns to input coordinates as to where they wish to place a mark. Coordinates are written in the form "x,y" where x is the column and y is the row. The program will only accept two integers separated by a comma. Once you have entered your coordinates, press the Enter/Return key. A new board will be displayed below with the updated spaces.
+
+   1,1 | 2,1 | 3,1
+   --- | --- | ---
+   1,2 | 2,2 | 3,2
+   --- | --- | ---
+   1,3 | 2,3 | 3,3
+
+  Coordinates can range from 1,1 to 3,3. Any values higher or lower will be caught as invalid by the program.
+
+*Stalemates
+  If all nine places have been filled and neither player has placed three marks in a row, the game has ended in a stalemate.
--- tictactoe.c
+++ tictactoe.c
@@ -17,132 +17,133 @@ int testDiagonal();
 int board[3][3];
 
 int main(int argc, char *argv[]) {
-  int turns = 0;
-  int currentPlayer;
-  int currentX;
-  int currentY;
-  char *shape[] = { "", "Noughts", "Crosses" };
+    int turns = 0;
+    int currentPlayer;
+    int currentX;
+    int currentY;
+    char *shape[] = { "", "Noughts", "Crosses" };
 
-  printf("\n   Tic Tac Toe\n   ===========\n\n");
-  clearBoard();
-  printBoard();
+    printf("\n   Tic Tac Toe\n   ===========\n\n");
+    clearBoard();
+    printBoard();
 
-  while(turns < 9) {
+    while (turns < 9) {
 
-    currentPlayer = (turns % 2) + 1;
-    printf("\n Player %d (%s), enter x,y coordinates for your move: ", currentPlayer, shape[currentPlayer]);
-    scanf("%d,%d", &currentX, &currentY);
+        currentPlayer = (turns % 2) + 1;
+        printf("\n Player %d (%s), enter x,y coordinates for your move: ", currentPlayer, shape[currentPlayer]);
+        scanf("%d,%d", &currentX, &currentY);
 
-    if (!makeMove(currentPlayer, currentX-1, currentY-1)) {
-      printf(" Invalid move.\n");
-    } else {
-      printf("\n");
-      printBoard();
-      if (testGoal()) {
-	printf("\n Player %d has won!\n\n", currentPlayer);
-	return 0;
-      }
-      turns++;
+        if (!makeMove(currentPlayer, currentX-1, currentY-1)) {
+            printf(" Invalid move.\n");
+        } else {
+            printf("\n");
+            printBoard();
+            if (testGoal()) {
+                printf("\n Player %d has won!\n\n", currentPlayer);
+                return 0;
+            }
+            turns++;
+        }
+
     }
-  }
 
-  printf("\n Stalemate!\n\n");
-  return 0;
+    printf("\n Stalemate!\n\n");
+    return 0;
 }
 
 int makeMove(int player, int x, int y) {
-  // Check if a move has already been made in that position
-  // or if the coordinates are out of bounds
-  if (board[y][x] > 0 || x < 0 || x > 2 || y < 0 || y > 2) {
-    return FALSE;
-  } else {
-    board[y][x] = player;
-    return TRUE;
-  }
+    // Do not allow a move that has already been made in that position
+    // - or coordinates that are out of bounds.
+    if (board[y][x] > 0 || x < 0 || x > 2 || y < 0 || y > 2) {
+        return FALSE;
+    } else {
+        board[y][x] = player;
+        return TRUE;
+    }
 }
 
 void clearBoard() {
-  int y, x;
-  for (y = 0; y < 3; y++) {
-    for (x = 0; x < 3; x++) {
-      board[y][x] = 0;
+    int y, x;
+    for (y = 0; y < 3; y++) {
+        for (x = 0; x < 3; x++) {
+            board[y][x] = 0;
+        }
     }
-  }
-  return;
+    return;
 }
 
 void printBoard() {
-  int y, x;
-  for (y = 0; y < 3; y++) {
+    int y, x;
+    for (y = 0; y < 3; y++) {
 
-    printf("   ");
-    for(x = 0; x < 3; x++) {
-      if (board[y][x] == 0) {
-	printf("   ");
-      } else if (board[y][x] == 1) {
-	printf(" O ");
-      } else if (board[y][x] == 2) {
-	printf(" X ");
-      }
+        printf("   ");
+        for(x = 0; x < 3; x++) {
+            if (board[y][x] == 0) {
+                printf("   ");
+            } else if (board[y][x] == 1) {
+                printf(" O ");
+            } else if (board[y][x] == 2) {
+                printf(" X ");
+            }
 
-      if (x < 2) {
-	printf("|");
-      }
+            if (x < 2) {
+                printf("|");
+            }
+        }
+        printf("\n");
+
+        if (y < 2) {
+            printf("   ---|---|---\n"); // Separator
+        } else {
+            break;
+        }
+
     }
-    printf("\n");
-
-    if (y < 2) {
-      printf("   ---|---|---\n"); // Separator
-    } else {
-      break;
-    }
-
-  }
-  return;
+    return;
 }
 
 int testGoal() {
-  // Run all tests sequentially
-  if (testHorizontal()) {
-    return TRUE;
-  } else if (testVertical()) {
-    return TRUE;
-  } else if (testDiagonal()) {
-    return TRUE;
-  } else {
-    return FALSE;
-  }
+    // Run all tests sequentially.
+    if (testHorizontal()) {
+        return TRUE;
+    } else if (testVertical()) {
+        return TRUE;
+    } else if (testDiagonal()) {
+        return TRUE;
+    } else {
+        return FALSE;
+    }
 }
 
 int testHorizontal() {
-  // Test for equality between every horizontally adjacent position (excluding zero)
-  int x;
-  for (x = 0; x < 3; x++) {
-    if (board[0][x] != 0 && board[0][x] == board[1][x] && board[1][x] == board[2][x]) {
-      return TRUE;
+    // Test for equality between each horizontally adjacent position (excluding equality to zero).
+    int x;
+    for (x = 0; x < 3; x++) {
+        if (board[0][x] != 0 && board[0][x] == board[1][x] && board[1][x] == board[2][x]) {
+            return TRUE;
+        }
     }
-  }
-  return FALSE;
+    return FALSE;
 }
 
 int testVertical() {
-  // Tests equality of vertical positions
-  int y;
-  for (y = 0; y < 3; y++) {
-    if (board[y][0] != 0 && board[y][0] == board[y][1] && board[y][1] == board[y][2]) {
-      return TRUE;
+    // Tests equality of vertical positions.
+    int y;
+    for (y = 0; y < 3; y++) {
+        if (board[y][0] != 0 && board[y][0] == board[y][1] && board[y][1] == board[y][2]) {
+            return TRUE;
+        }
     }
-  }
-  return FALSE;
+    return FALSE;
 }
 
 int testDiagonal() {
-  // Tests equality of the two diagonal positions
-  if (board[0][0] != 0 && board[0][0] == board[1][1] && board[1][1] == board [2][2]) {
-    return TRUE;
-  } else if (board[2][0] != 0 && board[2][0] == board[1][1] && board[1][1] == board [0][2]) {
-    return TRUE;
-  }
-  return FALSE;
+    // Tests equality of the two possible diagonal positions.
+    if (board[0][0] != 0 && board[0][0] == board[1][1] && board[1][1] == board [2][2]) {
+        return TRUE;
+    } else if (board[2][0] != 0 && board[2][0] == board[1][1] && board[1][1] == board [0][2]) {
+        return TRUE;
+    }
+    return FALSE;
 }
 
